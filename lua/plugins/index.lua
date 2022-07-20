8614e0bd7c439f7b6882881fbfb94e5159a25a67
Ahmed Rezk <ahmed.rezk@principlestudios.com>
2022-07-20 13:07:23 -0600

    Tabs/Buffers mananger instaillation

--- lua/plugins/index.lua
+++ lua/plugins/index.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    PACKER_BOOTSTRAP = fn.system {
+        "git",
+        "clone",
+        "--depth",
+        "1",
+        "https://github.com/wbthomason/packer.nvim",
+        install_path,
+    }
+    print "Installing packer close and reopen Neovim..."
+    vim.cmd [[packadd packer.nvim]]
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,59 +26,63 @@ vim.cmd [[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+    return
 end
 
 -- Have packer use a popup window
 packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
+    display = {
+        open_fn = function()
+            return require("packer.util").float { border = "rounded" }
+        end,
+    },
 }
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
-  use "wbthomason/packer.nvim"
+    -- My plugins here
+    use "wbthomason/packer.nvim"
 
-  -- Helpers
-  use "nvim-lua/plenary.nvim" -- require for LSP
+    -- Helpers
+    use "nvim-lua/plenary.nvim" -- require for LSP
 
 
-  -- LSP
-  use "neovim/nvim-lspconfig"
-  use "williamboman/nvim-lsp-installer"
-  use "tamago324/nlsp-settings.nvim"
-  use "jose-elias-alvarez/null-ls.nvim"
+    -- LSP
+    use "neovim/nvim-lspconfig"
+    use "williamboman/nvim-lsp-installer"
+    use "tamago324/nlsp-settings.nvim"
+    use "jose-elias-alvarez/null-ls.nvim"
 
-   -- Treesitter
-   -- parser generator language syntax
-   use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate", config = function() require("plugins._treesitter").setup() end }
+    -- Treesitter
+    -- parser generator language syntax
+    use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate",
+        config = function() require("plugins._treesitter").setup() end }
 
-  -- cmp plugins
-  use {"hrsh7th/nvim-cmp", config = function() require("plugins._cmp").setup() end}
-  use "hrsh7th/cmp-buffer"
-  use "hrsh7th/cmp-path"
-  use "hrsh7th/cmp-cmdline"
-  use "saadparwaiz1/cmp_luasnip"
-  use "hrsh7th/cmp-nvim-lsp"
-  use { "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp" }
-  use { "onsails/lspkind.nvim" }
-  use { "hrsh7th/cmp-nvim-lsp-signature-help" }
+    -- cmp plugins
+    use { "hrsh7th/nvim-cmp", config = function() require("plugins._cmp").setup() end }
+    use "hrsh7th/cmp-buffer"
+    use "hrsh7th/cmp-path"
+    use "hrsh7th/cmp-cmdline"
+    use "saadparwaiz1/cmp_luasnip"
+    use "hrsh7th/cmp-nvim-lsp"
+    use { "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp" }
+    use { "onsails/lspkind.nvim" }
+    use { "hrsh7th/cmp-nvim-lsp-signature-help" }
 
-  -- snippets
-  use { "L3MON4D3/LuaSnip" }
-  use "rafamadriz/friendly-snippets"
+    -- snippets
+    use { "L3MON4D3/LuaSnip" }
+    use "rafamadriz/friendly-snippets"
 
-  -- Lua interface plugins
-  use {"kyazdani42/nvim-tree.lua", config = function() require("plugins._nvim-tree").setup() end} -- Files exploer
-  use {"folke/which-key.nvim", config = function () require("plugins._whichkey").setup() end } -- shortcuts manager
+    -- Lua interface plugins
+    use { "kyazdani42/nvim-tree.lua", config = function() require("plugins._nvim-tree").setup() end } -- Files exploer
+    use { "folke/which-key.nvim", config = function() require("plugins._whichkey").setup() end } -- shortcuts manager
+    use { "akinsho/bufferline.nvim", config = function() require("plugins._bufferline").setup() end } -- Tabs/Buffers mananger
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+    use { "olimorris/onedarkpro.nvim", config = function() require("themes.onedarkPro").setup() end }
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if PACKER_BOOTSTRAP then
+        require("packer").sync()
+    end
 end)
---
+++ lua/themes/onedarkPro.lua
@@ -0,0 +1,127 @@
+local M = {}
+
+function M.setup()
+  local onedarkpro = require "onedarkpro"
+  onedarkpro.setup {
+    theme = function()
+      if vim.o.background == "dark" then
+        return "onedark"
+      else
+        return "onelight"
+      end
+    end,
+    colors = {
+      onelight = {
+        bg = "#f5f5f5", -- green
+      },
+      --[[ onedark = {
+        color_column = "#61afef",
+        cursorline = "#61afef",
+      }, ]]
+    }, -- Override default colors by specifying colors for 'onelight' or 'onedark' themes
+    hlgroups = {
+      PmenuSel = { bg = "#282C34", fg = "NONE" },
+      Pmenu = { fg = "#C5CDD9", bg = "#22252A" },
+
+      -- Start of cmp highlights
+      CmpItemAbbrDeprecated = { fg = "#7E8294", bg = "NONE", fmt = "strikethrough" },
+      CmpItemAbbrMatch = { fg = "#82AAFF", bg = "NONE", fmt = "bold" },
+      CmpItemAbbrMatchFuzzy = { fg = "#82AAFF", bg = "NONE", fmt = "bold" },
+      CmpItemMenu = { fg = "#C792EA", bg = "NONE", fmt = "italic" },
+
+      CmpItemKindField = { fg = "#EED8DA", bg = "#B5585F" },
+      CmpItemKindProperty = { fg = "#EED8DA", bg = "#B5585F" },
+      CmpItemKindEvent = { fg = "#EED8DA", bg = "#B5585F" },
+
+      CmpItemKindText = { fg = "#C3E88D", bg = "#9FBD73" },
+      CmpItemKindEnum = { fg = "#C3E88D", bg = "#9FBD73" },
+      CmpItemKindKeyword = { fg = "#C3E88D", bg = "#9FBD73" },
+
+      CmpItemKindConstant = { fg = "#FFE082", bg = "#D4BB6C" },
+      CmpItemKindConstructor = { fg = "#FFE082", bg = "#D4BB6C" },
+      CmpItemKindReference = { fg = "#FFE082", bg = "#D4BB6C" },
+
+      CmpItemKindFunction = { fg = "#EADFF0", bg = "#A377BF" },
+      CmpItemKindStruct = { fg = "#EADFF0", bg = "#A377BF" },
+      CmpItemKindClass = { fg = "#EADFF0", bg = "#A377BF" },
+      CmpItemKindModule = { fg = "#EADFF0", bg = "#A377BF" },
+      CmpItemKindOperator = { fg = "#EADFF0", bg = "#A377BF" },
+
+      CmpItemKindVariable = { fg = "#C5CDD9", bg = "#7E8294" },
+      CmpItemKindFile = { fg = "#C5CDD9", bg = "#7E8294" },
+
+      CmpItemKindUnit = { fg = "#F5EBD9", bg = "#D4A959" },
+      CmpItemKindSnippet = { fg = "#F5EBD9", bg = "#D4A959" },
+      CmpItemKindFolder = { fg = "#F5EBD9", bg = "#D4A959" },
+
+      CmpItemKindMethod = { fg = "#DDE5F5", bg = "#6C8ED4" },
+      CmpItemKindValue = { fg = "#DDE5F5", bg = "#6C8ED4" },
+      CmpItemKindEnumMember = { fg = "#DDE5F5", bg = "#6C8ED4" },
+
+      CmpItemKindInterface = { fg = "#D8EEEB", bg = "#58B5A8" },
+      CmpItemKindColor = { fg = "#D8EEEB", bg = "#58B5A8" },
+      CmpItemKindTypeParameter = { fg = "#D8EEEB", bg = "#58B5A8" },
+      -- End of cmp highlights
+
+      FloatBorder = {
+        fg = "#61afef",
+      },
+      TelescopeBorder = {
+        fg = "#61afef",
+      },
+      TelescopePromptBorder = {
+        fg = "#61afef",
+      },
+      TelescopePreviewBorder = {
+        fg = "#61afef",
+      },
+
+      VertSplit = {
+        fg = "${purple}",
+        -- bg = "${red}",
+      },
+      TSProperty = {
+        fg = (vim.o.background == "dark" and "${white}" or "${black}"),
+      },
+      TSTagAttribute = {
+        fg = "${purple}",
+      },
+      -- LspDiagnosticsDefaultWarning = { fg = "${white}", bg = "${yellow}" },
+      --[[ CmpDocumentation = {
+        bg = "#61afef",
+        fg = "#61afef",
+      },
+      CmpDocumentationBorder = {
+        bg = "#61afef",
+        fg = "#61afef",
+      }, ]]
+    },
+    filetype_hlgroups = {}, -- Override default highlight groups for specific filetypes
+    plugins = { -- Override which plugins highlight groups are loaded
+      all = true,
+      native_lsp = true,
+      polygot = true,
+      treesitter = true,
+    },
+    styles = {
+      comments = "italic",
+      keywords = "bold", -- change style of keywords to be bold
+      functions = "italic,bold", -- styles can be a comma separated list
+      strings = "none",
+      variables = "none",
+    },
+    options = {
+      bold = true, -- Use the themes opinionated bold styles?
+      italic = true, -- Use the themes opinionated italic styles?
+      underline = true, -- Use the themes opinionated underline styles?
+      undercurl = true, -- Use the themes opinionated undercurl styles?
+      cursorline = true, -- Use cursorline highlighting?
+      transparency = false, -- Use a transparent background?
+      terminal_colors = true, -- Use the theme's colors for Neovim's :terminal?
+      window_unfocussed_color = true, -- When the window is out of focus, change the normal background?
+    },
+  }
+  onedarkpro.load()
+end
+
+return M
