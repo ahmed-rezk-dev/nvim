bef668c316fc41ab9f0d25e3fa52e3f9e5179379
Ahmed Rezk <work72019@gmail.com>
2022-11-29 19:02:52 -0700

    update typecheck job

--- lua/utils/tasks.lua
+++ lua/utils/tasks.lua
@@ -2,7 +2,7 @@ local M = {}
 local overseer = require('overseer')
 M.runTypeCheckTasks = function()
     overseer.add_template_hook({ name= 'npm typecheck' }, function(task_defn, util)
-      util.add_component(task_defn, { "on_output_quickfix", open = false })
+      util.add_component(task_defn, { "on_output_quickfix", open = false, set_diagnostics = true,  items_only = true })
       util.add_component(task_defn, { "on_result_notify", infer_status_from_diagnostics = true })
     end)
 
